37e15e120707f88107c979fe6f9a932133ee3c51
Luci Temple <5368666+lucitemple@users.noreply.github.com>
2022-01-29 11:57:56 +1100

    add react NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Container, Navbar, Nav} from 'react-bootstrap';
+
+const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="lg" sticky="top">
+      <Container>
+        <Navbar.Brand href="#home">Reading Book Challege</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navigation-bar" />
+        <Navbar.Collapse id="navigation-bar">
+          <Nav className="mx-auto" id="centre-nav">
+            <Nav.Link href="#create">Create</Nav.Link>
+            <Nav.Link href="#challenges">Challenges</Nav.Link>
+            <Nav.Link href="#faq">FAQ</Nav.Link>
+          </Nav>
+          <Nav className="me-1" id="login-nav">
+            <Nav.Link href="#log-in">Log In</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
+
