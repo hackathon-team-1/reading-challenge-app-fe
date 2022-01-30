2438a8a5b0d93728ebcbfccd6e70e5f79a4c1d47
Luci Temple <5368666+lucitemple@users.noreply.github.com>
2022-01-30 18:45:30 +1100

    Add challenge cards to home page view

--- src/components/ChallengeList.jsx
+++ src/components/ChallengeList.jsx
@@ -1,25 +1,17 @@
 import React from "react";
-import { Container, Card, Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import {challengeData} from "../data/challengeData";
+import "././ChallengeList.css";
+import ChallengeCard from "./ChallengeCard";
 
 
 const ChallengeList = () => {
-    console.log(challengeData[0]);
+
   return (
-    <Container>
-      {challengeData.map((challenge) => (
-          
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={challenge.challengeImg} />
-          <Card.Body>
-            <Card.Title>{challenge.challengeName}</Card.Title>
-            <Card.Text>
-              {challenge.description}
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      ))}
+    <Container className="d-flex flex-wrap justify-content-center">
+      {challengeData.map((challenge, index) => (
+        <ChallengeCard challengeName={challenge.challengeName} challengeImg={challenge.challengeImg} deadline={challenge.deadline} description={challenge.description} registered={challenge.registered} link={challenge.link} key={"challenge"+index}/> 
+    ))}
     </Container>
   );
 };
--- src/data/challengeData.js
+++ src/data/challengeData.js
@@ -1,7 +1,8 @@
-let challengeData = [
+export let challengeData = [
   {
     challengeName: "Book Riot Read Harder 2022",
-    challengeImg: "https://i.imgur.com/XQQZQZL.png",
+    challengeImg:
+      "https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
     deadline: "31/12/2022",
     description:
       "24 Tasks. Push yourself to expand your horizons and break out of your comfortable reading bubble.",
@@ -10,7 +11,8 @@ let challengeData = [
   },
   {
     challengeName: "The 52 Book Club",
-    challengeImg: "https://i.imgur.com/XQQZQZL.png",
+    challengeImg:
+      "https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
     deadline: "31/12/2022",
     description:
       "Read 52 books from the 52 different categories provided – find a book that meets the criteria and check it off!",
@@ -19,7 +21,8 @@ let challengeData = [
   },
   {
     challengeName: "Fairytale Reading Challenge",
-    challengeImg: "https://i.imgur.com/XQQZQZL.png",
+    challengeImg:
+      "https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
     deadline: "31/06/2023",
     description:
       "Each month has a fairytale assigned to it, and you can choose any retelling of that fairytale to read for that month, or read the original fairytale.",
@@ -28,18 +31,22 @@ let challengeData = [
   },
   {
     challengeName: "Popsugar Reading Challenge",
-    challengeImg: "https://i.imgur.com/XQQZQZL.png",
+    challengeImg:
+      "https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
     deadline: "31/12/2022",
     description:
       "Read books that meet the 40 random fun and interesting prompts. For an extra challenge read books that meet the 10 advanced prompts.",
-      registered:11495,
-      link: "https://www.popsugar.com/challenge/read-books-that-meet-the-40-random-fun-and-interesting-prompts/",
+    registered: 11495,
+    link: "https://www.popsugar.com/challenge/read-books-that-meet-the-40-random-fun-and-interesting-prompts/",
   },
-    {challengeName: "Read Around the World: 2022 Travel Reading Challenge",
-    challengeImg: "https://i.imgur.com/XQQZQZL.png",
+  {
+    challengeName: "Read Around the World: 2022 Travel Reading Challenge",
+    challengeImg:
+      "https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
     deadline: "31/12/2022",
-    description:"12 prompts for 12 months that will allow you to read your way across seven continents and multiple genres.",
+    description:
+      "12 prompts for 12 months that will allow you to read your way across seven continents and multiple genres.",
     registered: 8565,
     link: "https://www.readaroundtheworld.com/challenge/read-around-the-world-2022-travel-reading-challenge/",
-}
+  },
 ];
