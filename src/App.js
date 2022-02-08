89c67d58fa75c6b4433417e715a55a0ae0524205
Dalila K Leo <dalilakatialeo@gmail.com>
2022-02-08 20:23:35 +1000

    rerouting

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import  NavBar from "./components/NavBar";
 import Home from "./components/Home";
-import  LoginModal  from "./components/LoginModal";
 // import { RegistrationModal } from "./components/RegistrationModal";
 import UserChallengePage from "./components/UserChallengePage";
 
@@ -13,7 +12,6 @@ const App = () => {
         <div>
         <Routes>
           <Route path="/user-challenge" element={<UserChallengePage/>}/>
-          <Route path="/" element={<Home/>} />
           <Route path="/reading-challenge-app-fe" element={<Home/>} />
         </Routes>
         </div>
